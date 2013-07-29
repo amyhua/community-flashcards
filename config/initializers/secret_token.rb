3460bc8d660b7d58595addf4625da32b27ba0407
Amy Hua <foramyhua@gmail.com>
2013-07-29 02:09:24 -0700

    Generated Rails 3 App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommunityFlashcards::Application.config.secret_token = '783136a00a1ea7c51d7c8fd810e3b2b61a9be5ce1ab730bbb776e43309aec2ee3dba6959f6192cfab0cad9d5096a0ad30f74dfb7e30324524a8da972e043957b'
